1775f75a0d94be4a8bcf5e1a77f172d16ebbd82e
Tuure Ilmarinen <tuure.ilmarinen@helsinki.fi>
2016-03-02 14:53:40 +0200

    test Google auth commit for heroku

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
 #provider :twitter, ENV['TWITTER_KEY'], ENV['TWITTER_SECRET']
 	provider :google_oauth2, ENV["GOOGLE_CLIENT_ID"], ENV["GOOGLE_SECRET"],
-		scope: 'profile', access_type: 'online'
+		         scope: 'profile', image_aspect_ratio: 'square', image_size: 48, access_type: 'online', name: 'google'
 end
