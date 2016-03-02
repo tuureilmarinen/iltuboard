34045f90637de2e09c30ad05956996e244a264cb
Tuure Ilmarinen <tuure.ilmarinen@helsinki.fi>
2016-03-02 15:59:46 +0200

    fixed login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,9 @@
 class User < ActiveRecord::Base
 	enum status: [:user, :moderator, :admin]
-	#class << self
-	def from_omniauth(auth_hash)
-		user = find_or_create_by(uid: auth_hash['uid'], provider: auth_hash['provider'])
-		user.name = auth_hash['info']['name']
-		#user.url = auth_hash['info']['urls']['Twitter']
+	def self.from_omniauth(auth_hash)
+		user = find_or_create_by(uid: auth_hash['uid'], provider: auth_hash['provider'], name: auth_hash['info']['name'])
+		user.url = auth_hash['info']['urls'][user.provider.capitalize]
 		user.save!
 		user
 	end
-
-#end
 end
