c1d16c28a7045494712c4ee8c2452571846e8f4c
Tuure Ilmarinen <tuure.ilmarinen@helsinki.fi>
2016-03-15 20:03:52 +0200

    added find_topics_sorted

--- app/models/board.rb
+++ app/models/board.rb
@@ -3,5 +3,9 @@ class Board < ActiveRecord::Base
 	has_many :posts, through: :topics
 	validates :url, uniqueness: { case_sensitive: false }
 	validates :url, exclusion: { in: %w(boards topics posts users),
-    message: "%{value} is reserved." }
+    	message: "%{value} is reserved." }
+
+    def find_topics_sorted
+		Topic.find_by_sql ["select distinct * from (select topics.* from topics join posts on posts.topic_id=topics.id where topics.board_id=1 order by posts.created_at asc);",self.id]
+    end
 end
