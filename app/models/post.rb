091451b386b7fe55f7c0727a9e037df043b9b821
Tuure Ilmarinen <tuure.ilmarinen@helsinki.fi>
2016-03-15 22:29:16 +0200

    modified topnav

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,17 +4,15 @@ class Post < ActiveRecord::Base
 	belongs_to :user
 	has_one :ban
 	delegate :board, to: :topic
-	#has_many :post_attachments
-	#accepts_nested_attributes_for :post_attachments
 	mount_uploader :attachment, AttachmentUploader
 	
 	validates :content, presence: true
-	#validates :topic, presence: true
+	#validates :topic, presence: true # currently not in use because accepting nested attributes in topic wont work otherwise
 	validates :show_name, presence: true
 
 	def banned?
 		not self.ban.nil?
-	end
+	end§
 	def url
 		self.topic.url+"#"+self.id.to_s
 	end
